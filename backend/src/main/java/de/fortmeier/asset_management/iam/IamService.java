66992cd837df89c58522e5f72becb2a4dae8d2c4
Marc Fortmeier <mf@freund.eu>
2024-06-03 08:39:41 +0200

    pipeline test

--- backend/src/main/java/de/fortmeier/asset_management/iam/IamService.java
+++ backend/src/main/java/de/fortmeier/asset_management/iam/IamService.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
